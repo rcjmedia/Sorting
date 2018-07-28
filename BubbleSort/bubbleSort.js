91a27ac845e6f515d88702743c62727969578d4f
Romeo <romeocorpuz808@gmail.com>
2018-07-28 10:48:23 -1000

    fixed

--- BubbleSort/bubbleSort.js
+++ BubbleSort/bubbleSort.js
@@ -1,53 +1,23 @@
-// var mix = [3, 6, 1, 8, 2, 4, 9, 5, 7];
+let mixArr = [7, 5, 8, 6, 1, 9];
 
-// function quickSort(arr){
-//   let pivot = arr[0];
-//   let left = [];
-//   let right = [];
-  
-//   if(arr.length < 2){
-//     return arr;
-//   }
-//   else{
-    
-//      for(let i = 1; i< arr.length; i++){
-//       if(arr[i] <= pivot){
-//         left.push(arr[i]);
-//       }else{
-//         right.push(arr[i]);
-//       }
-//      }
-//   }
-//       return quickSort(left).concat(pivot, quickSort(right));  
+function swap(mixArr, i, j) {
+    let temp = mixArr[i];
+    mixArr[i] = mixArr[j];
+    mixArr[j] = temp;
+  }
 
-// }
-// console.log(quickSort(mix));
+function bubbleSort(mixArr) {
+    let swapped;
+    do {
+        swapped = null;
+      for(var i = 0; i < mixArr.length; i++) {
+        if (mixArr[i] > mixArr[i + 1] && mixArr[i + 1] !== null) {
+          swap(mixArr, i, i + 1);
+          swapped = true;
+        }
+      }
+    } while(swapped);
+    return mixArr;
+  }
 
-// var mix = [3, 6, 1, 8, 2, 4, 9, 5, 7];
-
-// function quickSort1(array) {
-//   function quickSort2(array, left, right) {
-//     var i = left;
-//     var j = right;
-//     var pivot = array[Math.floor((left+right)/2)];
-//     //dividing it by two then round to the closest num and count the index.
-
-//     while (i <= j) {
-//       while (array[i] < pivot) i++;
-//       while (array[j] > pivot) j--;
-
-//       if (i <= j) {
-//         var temp = array[i];
-//         array[i] = array[j];
-//         array[j] = temp;
-//         i++;
-//         j--;
-//       }
-//     }
-//     if (left < j) quickSort2(array, left, j);
-//     if (i < right) quickSort2(array, i, right);
-//     return array;
-//   }
-//   return quickSort2(array, 0, array.length-1);  
-// }
-// console.log(quickSort1(mix));
+  console.log(bubbleSort(mixArr));
